bed41017bd1152776dbdfec9f276637f5a830e1e
Jonathan <jjs1mail@gmail.com>
2018-01-21 16:38:26 -0600

    moved endpoint for HelloController to /SpringHello, angular is now served at the index (e.g. localhost:8080)

--- src/main/java/org/csc394/employeematch/HelloController.java
+++ src/main/java/org/csc394/employeematch/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/SpringHello")
     public String index(){
         return "Hello CSC 394 Teammates (from Spring)!";
     }
