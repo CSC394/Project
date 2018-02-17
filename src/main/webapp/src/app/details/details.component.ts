d0c728371c538d0013cb3413d13a63a3d6650169
Matt Hallowell <matthewhallowell1@gmail.com>
2018-02-17 17:21:57 -0600

    created details component

---
+++ src/main/webapp/src/app/details/details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'details',
+    templateUrl: 'details.component.html',
+    styleUrls: ['details.component.css']
+})
+export class DetailsComponent {
+
+}
---
+++ src/main/webapp/src/app/details/details.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { DetailsComponent } from './details.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        DetailsComponent,
+    ],
+    exports: [
+        DetailsComponent,
+    ]
+})
+export class DetailsModule {
+
+}
