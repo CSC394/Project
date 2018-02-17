d45564f91be616fea87031cc6e553a15894484b1
Matt Hallowell <matthewhallowell1@gmail.com>
2018-02-17 15:50:30 -0600

    added jobs component

---
+++ src/main/webapp/src/app/jobs/jobs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'jobs',
+    templateUrl: 'jobs.component.html',
+    styleUrls: ['jobs.component.css']
+})
+export class JobsComponent {
+
+}
---
+++ src/main/webapp/src/app/jobs/jobs.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { JobsComponent } from './jobs.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        JobsComponent,
+    ],
+    exports: [
+        JobsComponent,
+    ]
+})
+export class JobsModule {
+
+}
